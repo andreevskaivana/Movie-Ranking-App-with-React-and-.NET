760da9dfd2ad4e5dc41c2f82790ec6d3d6ba1e70
andreevskaivana <ivana.andreevska25@gmail.com>
2023-08-31 23:25:01 +0200

    Add project files.

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,59 @@
+﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
+import ItemCollection from './ItemCollection';
+
+const RankItems = () => {
+
+    const [reload, setReload] = useState(false);
+
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    function Reload() {
+        setReload(true);
+    }
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+    }
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            });
+    }, []);
+
+    return (
+        (items != null) ?
+            <main>
+                <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+                <ItemCollection items={items} drag={drag} imgArr={MovieImageArr} />
+                <button onClick={Reload} className="reload" style={{ "marginTop": "10px" }}> <span className="text" >Reload</span > </button>
+            </main>
+            : <main>Loading...</main>
+    );
+}
+
+export default RankItems;
---
+++ RankingApp/Models/ItemModel.cs
@@ -0,0 +1,18 @@
+﻿namespace RankingApp.Models
+{
+    public class ItemModel
+    {
+        public int Id {get;set;}
+        public string Title {get;set;}  
+        //each item has an associated image -> item = one image
+        //one to one relation with image
+        public int ImageId { get;set;}
+
+        //ranking value to an item
+        public int Ranking { get;set;}
+
+        //item type(movie=1,album=2
+        //one to many relation with items 
+        public int ItemType { get; set; }
+    }
+}
